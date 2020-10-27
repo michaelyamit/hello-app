b0f71de3f258a6d0818d22d0a428ba7e710bfb9d
michaelyamit <32030520+michaelyamit@users.noreply.github.com>
2020-10-27 09:43:46 +0200

    Update main.go

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ func main() {
 func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving request: %s", r.URL.Path)
 	host, _ := os.Hostname()
-	fmt.Fprintf(w, "Bye Uri Peled!\n")
+	fmt.Fprintf(w, "Bye Uri Peled!!\n")
 	fmt.Fprintf(w, "Version: 2.0.0\n")
 	fmt.Fprintf(w, "Hostname: %s\n", host)
 }
